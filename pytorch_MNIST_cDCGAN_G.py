970b87c24f57d29fa598555d099402f0822bf867
xiaoyubing <422749700@qq.com>
2019-01-06 22:11:25 +0800

    增加适配CPU生成数据,指定生成数字

--- pytorch_MNIST_cDCGAN_G.py
+++ pytorch_MNIST_cDCGAN_G.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import numpy as np
+import cv2
 
 # G(z)
 class generator(nn.Module):
@@ -52,32 +54,104 @@ with torch.no_grad():
         fixed_z_, fixed_y_label_ = Variable(fixed_z_.cuda()), Variable(fixed_y_label_.cuda())
     else:
         fixed_z_, fixed_y_label_ = Variable(fixed_z_), Variable(fixed_y_label_)
-def show_result(num_epoch, show = False, save = False, path = 'result.png'):
+def generate_num(num_str,show = False, save = False, path = 'result.png'):
+    G.eval()
+    test_images = G(fixed_z_, fixed_y_label_)  # shape [100,1,32,32]
+    test_images = test_images.reshape(10, 10, 32, 32)
+    print(len(num_str))
+    for style_idx in range(10):
+        for idx, x in enumerate(num_str):
+            num = int(x)
+            # 保存单字的图片
+            tmp_img = test_images[num, style_idx].cpu().data.numpy()
+            tmp_img = np.maximum(tmp_img, 0)
+            tmp_img = tmp_img * 255
+            tmp_img = np.around(tmp_img)
+            if idx == 0:
+                img_gray = tmp_img.astype(np.uint8)
+            else:
+                ###hstack()在行上合并
+                img_gray = np.hstack((img_gray, tmp_img.astype(np.uint8)))
+        ret, thresh = cv2.threshold(
+            img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        if save:
+            generate_num_path = root + 'Fixed_results/generate_num' + str(style_idx) + '.png'
+            cv2.imwrite(generate_num_path, thresh)
+        if show:
+            cv2.imshow('2', thresh)
+            cv2.waitKey()
+
+
+def show_result(num_str, show = False, save = False, path = 'result.png'):
+    # img = cv2.imread('lena.jpg')
+    #
+    # # 1.转成灰度图
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #
+    # cv2.imshow('img', img)
+    # cv2.imshow('gray', img_gray)
+    # cv2.waitKey(0)
 
     G.eval()
-    test_images = G(fixed_z_, fixed_y_label_)
-    G.train()
+    test_images = G(fixed_z_, fixed_y_label_) # shape [100,1,32,32]
+    test_images = test_images.reshape(10, 10, 32, 32)
+    print(len(num_str))
+    for idx,x in enumerate(num_str):
+        num = int(x)
+        # 保存单字的图片
+        tmp_img = test_images[num, 0].cpu().data.numpy()
+        tmp_img = np.maximum(tmp_img, 0)
+        tmp_img = tmp_img * 255
+        tmp_img = np.around(tmp_img)
+        if idx == 0:
+            img_gray = tmp_img.astype(np.uint8)
+        else:
+            ###hstack()在行上合并
+            img_gray = np.hstack((img_gray, tmp_img.astype(np.uint8)))
+    ret, thresh = cv2.threshold(
+        img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    cv2.imwrite('result.jpg', thresh)
 
-    size_figure_grid = 10
-    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
-    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
-        ax[i, j].get_xaxis().set_visible(False)
-        ax[i, j].get_yaxis().set_visible(False)
+    # 保存单字的图片
+    tmp_img = test_images[0, 0].cpu().data.numpy()
+    tmp_img= np.maximum(tmp_img, 0)
+    tmp_img = tmp_img * 255
+    tmp_img = np.around(tmp_img)
+    img_gray = tmp_img.astype(np.uint8)
 
-    for k in range(10*10):
-        i = k // 10
-        j = k % 10
-        ax[i, j].cla()
-        ax[i, j].imshow(test_images[k, 0].cpu().data.numpy(), cmap='gray')
+    cv2.imwrite('single-test.jpg', img_gray)
+    # img = cv2.imread('single-test.jpg')
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 使用Otsu自动阈值，注意用的是cv2.THRESH_BINARY_INV
+    ret, thresh = cv2.threshold(
+        img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    cv2.imwrite('single-test111.jpg', thresh)
+    cv2.imshow('2', thresh)
+    cv2.waitKey()
 
-    label = 'Epoch {0}'.format(num_epoch)
-    fig.text(0.5, 0.04, label, ha='center')
-    plt.savefig(path)
-
-    if show:
-        plt.show()
-    else:
-        plt.close()
+    # size_figure_grid = 10
+    # fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
+    # for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+    #     ax[i, j].get_xaxis().set_visible(False)
+    #     ax[i, j].get_yaxis().set_visible(False)
+    #
+    # for k in range(10*10):
+    #     i = k // 10
+    #     j = k % 10
+    #     ax[i, j].cla()
+    #     ax[i, j].imshow(test_images[k, 0].cpu().data.numpy(), cmap='gray')
+    #     # plt.imshow(test_images[k, 0].cpu().data.numpy())  # Needs to be in row,col order
+    #     # fixed_p = root + 'Fixed_results/test' + str(k) + '.png'
+    #     # plt.savefig(fixed_p)
+    #
+    # label = 'Epoch {0}'.format(num_epoch)
+    # fig.text(0.5, 0.04, label, ha='center')
+    # plt.savefig(path)
+    #
+    # if show:
+    #     plt.show()
+    # else:
+    #     plt.close()
 
 
 # network
@@ -96,7 +170,8 @@ if torch.cuda.is_available():
 else:
     G.load_state_dict(torch.load(root + model + 'generator_param.pkl', map_location='cpu'))
 
-flag = 'test_G_3'
-fixed_p = root + 'Fixed_results/' + model + flag + '.png'
-show_result(flag, save=True, path=fixed_p)
+num_str = '15228842534'
+fixed_p = root + 'Fixed_results/' + model + str(num_str) + '.png'
+# show_result(num_str, save=True, path=fixed_p)
+generate_num(num_str, save=True, path=fixed_p)
 
